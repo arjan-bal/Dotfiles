0ea2533c7033e2d8655d355f865d7c5f01d4958c
Arjan Bal <arjansbal@google.com>
2024-12-12 00:37:41 +0530

    Telescope and treesitter improvements

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,28 +1,41 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-    },
-    keys = {
-        { '<leader>ff',  function() require('telescope.builtin').find_files() end,                desc = 'Find Files' },
-        { '<leader>fg',  function() require('telescope.builtin').live_grep() end,                 desc = 'Live Grep' },
-        { '<leader>fb',  function() require('telescope.builtin').buffers() end,                   desc = 'Buffers' },
-        { '<leader>fh',  function() require('telescope.builtin').help_tags() end,                 desc = 'Help Tags' },
-        { '<leader>fo',  function() require('telescope.builtin').lsp_document_symbols() end,      desc = 'Document Symbols' },
-        { '<leader>fc',  function() require('telescope.builtin').commands() end,                  desc = 'Commands' },
-        { '<leader>fz',  function() require('telescope.builtin').current_buffer_fuzzy_find() end, desc = 'Fuzzy Find in Buffer' },
-        { '<leader>fgb', function() require('telescope.builtin').git_branches() end,              desc = 'Git Branch' },
-    },
-    opts = function()
-        return {
-            defaults = {
-                mappings = {
-                    n = {
-                        ['dd'] = require('telescope.actions').delete_buffer,
+    {
+        'nvim-telescope/telescope.nvim',
+        branch = '0.1.x',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope-fzf-native.nvim'
+        },
+        keys = {
+            { '<leader>ff', function() require('telescope.builtin').find_files() end,                desc = 'Find Files' },
+            { '<leader>fg', function() require('telescope.builtin').live_grep() end,                 desc = 'Live Grep' },
+            { '<leader>fb', function() require('telescope.builtin').buffers() end,                   desc = 'Buffers' },
+            { '<leader>fh', function() require('telescope.builtin').help_tags() end,                 desc = 'Help Tags' },
+            { '<leader>fo', function() require('telescope.builtin').lsp_document_symbols() end,      desc = 'Document Symbols' },
+            { '<leader>fc', function() require('telescope.builtin').commands() end,                  desc = 'Commands' },
+            { '<leader>fz', function() require('telescope.builtin').current_buffer_fuzzy_find() end, desc = 'Fuzzy Find in Buffer' },
+            { '<leader>fr', function() require('telescope.builtin').git_branches() end,              desc = 'Git Branch' },
+        },
+        config = function()
+            local telescope = require('telescope')
+            telescope.setup {
+                defaults = {
+                    mappings = {
+                        n = {
+                            ['dd'] = require('telescope.actions').delete_buffer,
+                        },
                     },
                 },
-            },
-        }
-    end,
+                extensions = {
+                    fzf = {}
+                }
+
+            }
+            telescope.load_extension('fzf')
+        end,
+    },
+    {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
+    }
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -7,6 +7,13 @@ return {
             -- Automatically install missing parsers when entering buffer
             -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
             auto_install = true,
+            disable = function(lang, buf)
+                local max_filesize = 100 * 1024 -- 100 KB
+                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                if ok and stats and stats.size > max_filesize then
+                    return true
+                end
+            end,
         }
     end
 }
