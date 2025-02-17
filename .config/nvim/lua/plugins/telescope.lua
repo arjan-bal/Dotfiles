f86a378d6b4bc074291b127d7c88accede0128a4
Arjan Bal <arjansbal@google.com>
2025-02-17 23:45:04 +0530

    Telescope shortcut for marks

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -8,13 +8,13 @@ return {
         },
         keys = {
             { '<leader>ff', function() require('telescope.builtin').find_files() end,                desc = 'Find Files' },
-            -- { '<leader>fg', function() require('telescope.builtin').live_grep() end,                 desc = 'Live Grep' },
             { '<leader>fb', function() require('telescope.builtin').buffers() end,                   desc = 'Buffers' },
             { '<leader>fh', function() require('telescope.builtin').help_tags() end,                 desc = 'Help Tags' },
             { '<leader>fo', function() require('telescope.builtin').lsp_document_symbols() end,      desc = 'Document Symbols' },
             { '<leader>fc', function() require('telescope.builtin').commands() end,                  desc = 'Commands' },
             { '<leader>fz', function() require('telescope.builtin').current_buffer_fuzzy_find() end, desc = 'Fuzzy Find in Buffer' },
             { '<leader>fr', function() require('telescope.builtin').git_branches() end,              desc = 'Git Branch' },
+            { '<leader>fm', function() require('telescope.builtin').marks() end,              desc = 'Marks' },
         },
         config = function()
             local telescope = require('telescope')
